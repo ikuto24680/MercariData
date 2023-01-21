9655af2a1f2d95b6d8e02c9a98945015aeeacfb3
ikuto24680 <siutikuto@gamil.com>
2023-01-21 12:48:58 +0900

    Userドメインを使ってコンポジションをお試し

--- src/main/java/com/example/demo/domain/LoginUser.java
+++ src/main/java/com/example/demo/domain/LoginUser.java
@@ -5,77 +5,106 @@ import java.util.Collection;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public class LoginUser implements UserDetails{
-	
-	/**  ID*/
+public class LoginUser implements UserDetails {
+
+	/** ID */
 	private Integer id;
-	/**  名前*/
-	private String name;
-	/**  パスワード*/
-	private String password;
-	/**  権限*/
+//	/**  名前*/
+//	private String name;
+//	/**  パスワード*/
+//	private String password;
+	private User user;
+	/** 権限 */
 	private Integer authority;
+
+	public LoginUser(String name,String password, Integer id, Integer authority) {
+		user.setName(name);
+		user.setPassword(password);
+		this.id = id;
+		this.authority = authority;
+	}
+	
+	public LoginUser() {
+		
+	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
+	public void setUser(String name,String password) {
+		user.setName(name);
+		user.setPassword(password);
+	}
+
 	public String getName() {
-		return name;
+		return user.getName();
 	}
+
 	public void setName(String name) {
-		this.name = name;
+		user.setName(name);
+		;
 	}
+
 	public String getPassword() {
-		return password;
+		return user.getPassword();
 	}
+
 	public void setPassword(String password) {
-		this.password = password;
+		user.setPassword(password);
 	}
+
 	public Integer getAuthority() {
 		return authority;
 	}
+
 	public void setAuthority(Integer authority) {
 		this.authority = authority;
 	}
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", password=" + password + ", authority=" + authority + "]";
-	}
+
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public String getUsername() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public boolean isAccountNonExpired() {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public boolean isAccountNonLocked() {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public boolean isCredentialsNonExpired() {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public boolean isEnabled() {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	public Collection<? extends GrantedAuthority> roleList() {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 }
